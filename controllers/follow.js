303636e5ea0b48ca275ddb46edf74ddfa6e78a63
getc2 <getc2@192.168.1.14>
2019-11-02 17:47:35 -0400

    Clase 32

--- controllers/follow.js
+++ controllers/follow.js
@@ -32,7 +32,38 @@ function deleteFollow(req, res){
         return res.status(200).send({message: 'El follow se ha eliminado'});
     });
 }
+
+function getFollowingUsers(req, res){
+    var userId = req.user.sub;
+    
+    if(req.params.id && req.params.page){
+        userId = req.params.id;    
+    }
+    
+    var page = 1;
+    
+    if(req.params.page){
+        page = req.params.page;
+    }else{
+        page = req.params.id;
+    }
+    
+    var itemsPerPage = 4;
+    
+    Follow.find({user:userId}).populate({path: 'followed'}).paginate(page, itemsPerPage, (err, follows, total)=> {
+        if(err) return res.status(500).send({message: 'Error en el servidor'});
+        
+        if(!follows) return res.status(404).send({message: 'No estas siguiendo a ningun usuario'});
+        
+        return res.status(200).send({
+            total:total,
+            pages:Math.ceil(total/itemsPerPage),
+            follows
+        });
+    });
+}
 module.exports = {
     saveFollow,
-    deleteFollow
+    deleteFollow,
+    getFollowingUsers
 }
