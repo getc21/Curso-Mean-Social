f2bf0d52e6eda54009596d3e87cea19e32854b9d
getc2 <getc2@192.168.1.14>
2019-11-07 13:36:38 -0400

    Clase 40

--- controllers/publication.js
+++ controllers/publication.js
@@ -15,6 +15,28 @@ function probando(req, res) {
     });
 }
 
+function savePublication(req, res){
+    var params = req.body;
+    
+    var publication = new Publication();
+    
+    if(!params.text) return res.status(200).send({message: 'Debes enviar un texto!!'});
+    
+    var publication = new Publication();
+    publication.text = params.text;
+    publication.file = 'null';
+    publication.user = req.user.sub;
+    publication.created_at = moment().unix();
+    
+    publication.save((err, publicationStored) => {
+        if(err) return res.status(500).send({message: 'Error al guardar la publicacion'});
+        
+        if(!publicationStored) return res.send(404).send({message: 'La publicacion no ha sido guardada'});
+        
+        return res.status(200).send({publication: publicationStored});
+    });
+}
 module.exports = {
-    probando
+    probando,
+    savePublication
 }
