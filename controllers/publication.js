6c0e8fd104570b6f488550503ff92c39e6a7d626
getc2 <getc2@192.168.1.14>
2019-11-10 15:44:54 -0400

    Clase 44

--- controllers/publication.js
+++ controllers/publication.js
@@ -15,54 +15,60 @@ function probando(req, res) {
     });
 }
 
-function savePublication(req, res){
+function savePublication(req, res) {
     var params = req.body;
-    
+
     var publication = new Publication();
-    
-    if(!params.text) return res.status(200).send({message: 'Debes enviar un texto!!'});
-    
+
+    if (!params.text)
+        return res.status(200).send({message: 'Debes enviar un texto!!'});
+
     var publication = new Publication();
     publication.text = params.text;
     publication.file = 'null';
     publication.user = req.user.sub;
     publication.created_at = moment().unix();
-    
+
     publication.save((err, publicationStored) => {
-        if(err) return res.status(500).send({message: 'Error al guardar la publicacion'});
-        
-        if(!publicationStored) return res.send(404).send({message: 'La publicacion no ha sido guardada'});
-        
+        if (err)
+            return res.status(500).send({message: 'Error al guardar la publicacion'});
+
+        if (!publicationStored)
+            return res.send(404).send({message: 'La publicacion no ha sido guardada'});
+
         return res.status(200).send({publication: publicationStored});
     });
 }
 
-function getPublications(req, res){
+function getPublications(req, res) {
     var page = 1;
-    if(req.params.page){
+    if (req.params.page) {
         page = req.params.page;
-        
+
     }
-    
+
     var itemsPerPage = 4;
-    
+
     Follow.find({user: req.user.sub}).populate('followed').exec((err, follows) => {
-        if(err) return res.status(500).send({message: 'Error al devolver el seguimiento'});
-        
+        if (err)
+            return res.status(500).send({message: 'Error al devolver el seguimiento'});
+
         var follows_clean = [];
-        
+
         follows.forEach((follow) => {
             follows_clean.push(follow.followed);
-            
+
         });
         Publication.find({user: {"$in": follows_clean}}).sort('-created_at').populate('user').paginate(page, itemsPerPage, (err, publications, total) => {
-        if(err) return res.status(500).send({message: 'Error al devolver publicaciones'});
-        
-            if(!publications) return res.status(404).send({message: 'No hay publicaciones'});
-            
+            if (err)
+                return res.status(500).send({message: 'Error al devolver publicaciones'});
+
+            if (!publications)
+                return res.status(404).send({message: 'No hay publicaciones'});
+
             return res.status(200).send({
                 total_items: total,
-                pages: Math.ceil(total/itemsPerPage),
+                pages: Math.ceil(total / itemsPerPage),
                 page: page,
                 publications
             })
@@ -70,27 +76,94 @@ function getPublications(req, res){
     });
 }
 
-function getPublication(req, res){
+function getPublication(req, res) {
     var publicationId = req.params.id;
-    
+
     Publication.findById(publicationId, (err, publication) => {
-        if(err) return res.status(500).send({message: 'Error al devolver publicaciones'});
-        
-        if(!publication) return res.status(404).send({message: 'No existe la publicacion'});
-        
-        res.status(200).send({publication});
+        if (err)
+            return res.status(500).send({message: 'Error al devolver publicaciones'});
+
+        if (!publication)
+            return res.status(404).send({message: 'No existe la publicacion'});
+
+        return res.status(200).send({publication});
     });
 }
 
-function deletePublication(req, res){
+function deletePublication(req, res) {
     var publicationId = req.params.id;
-    
+
     Publication.find({'user': req.user.sub, '_id': publicationId}).remove(err => {
-        if(err) return res.status(500).send({message: 'Error al borrar publicaciones'});
-        
-        if(!publicationRemoved) return res.status(404).send({message: 'No se ha borrado la publicacion'});
-        
+        if (err)
+            return res.status(500).send({message: 'Error al borrar publicaciones'});
+
+        if (!publicationRemoved)
+            return res.status(404).send({message: 'No se ha borrado la publicacion'});
+
         return res.status(200).send({message: 'Publicacion eliminada correctamente'});
+    });
+}
+
+
+//SUBIR ARCHIVOS DE IMAGEN/AVATAR DE USUARIO
+function uploadImage(req, res) {
+    var userId = req.params.id;
+     var publicationId = req.params.id;
+
+
+    if (req.files) {
+        var file_path = req.files.image.path;
+        var file_split = file_path.split('\\');
+        var file_name = file_split[2];
+        var ext_split = file_name.split('\.');       
+        var file_ext = ext_split[1];
+
+
+        if (file_ext == 'png' || file_ext == 'jpg' || file_ext == 'jpeg' || file_ext == 'gif') {
+
+            Publication.findOne({'user': req.user.sub, '_id': publicationId}).exec((err, publication) => {
+                console.log(publication);
+                if (publication) {
+                    //ACTUALIZAR DOCUMENTO DE LA PUBLICATION
+                    Publication.findByIdAndUpdate(publicationId, {file: file_name}, {new : true}, (err, publicationUpdated) => {
+                        if (err)
+                            return res.status(500).send({message: 'Error en la peticion'});
+
+                        if (!publicationUpdated)
+                            return res.status(404).send({message: 'No se ha podido actualizar el usuario'});
+
+                        return res.status(200).send({publication: publicationUpdated});
+                    });
+                } else {
+                    return removeFilesOfUploads(res, file_path, 'No tienes permiso para actualizar esta publicacion');
+                }
+            });
+            
+        } else {
+            return removeFilesOfUploads(res, file_path, 'Extension no valida');
+        }
+    } else {
+        return res.status(200).send({message: 'No se han subido imagenes'});
+    }
+}
+
+function removeFilesOfUploads(res, file_path, message) {
+    fs.unlink(file_path, (err) => {
+        return res.status(200).send({message: message});
+    });
+}
+
+//DEVOLVER IMAGEN DE USUARIO
+function getImageFile(req, res) {
+    var image_file = req.params.imageFile;
+    var path_file = './uploads/publications/' + image_file;
+
+    fs.exists(path_file, (exists) => {
+        if (exists) {
+            res.sendFile(path.resolve(path_file));
+        } else {
+            res.status(200).send({message: 'No existe la imagen...'});
+        }
     });
 }
 module.exports = {
@@ -98,5 +171,8 @@ module.exports = {
     savePublication,
     getPublications,
     getPublication,
-    deletePublication
+    deletePublication,
+    uploadImage,
+    removeFilesOfUploads,
+    getImageFile
 }
