3bb22d4cb2b6c6697c9500c7e51a5867579006a4
getc2 <getc2@192.168.1.14>
2019-11-13 17:18:20 -0400

    Clase 51

--- routes/message.js
+++ routes/message.js
@@ -8,4 +8,5 @@ var md_auth = require('../middlewares/authenticated');
 api.get('/probando-md', md_auth.ensureAuth, MessageController.probando);
 api.post('/message', md_auth.ensureAuth, MessageController.saveMessage);
 api.get('/my-messages/:page?', md_auth.ensureAuth, MessageController.getReceivedMessages);
+api.get('/messages/:page?', md_auth.ensureAuth, MessageController.getEmmitMessages);
 module.exports = api;
