3985585fa5625dead57d24c21219e9f6bcfc8a31
getc2 <getc2@192.168.1.14>
2019-11-13 15:43:08 -0400

    Clase 49

--- controllers/message.js
+++ controllers/message.js
@@ -7,31 +7,58 @@ var User = require('../models/user');
 var Follow = require('../models/follow');
 var Message = require('../models/message');
 
-function probando(req, res){
+function probando(req, res) {
     res.status(200).send({message: 'Hola que tal'});
 }
 
-function saveMessage(req, res){
+function saveMessage(req, res) {
     var params = req.body;
-    
-    if(!params.text || !params.receiver)
+
+    if (!params.text || !params.receiver)
         return     res.status(200).send({message: 'Envia los datos necesarios'});
-    
+
     var message = new Message();
     message.emitter = req.user.sub;
     message.receiver = params.receiver;
     message.text = params.text;
     message.created_at = moment().unix();
-    
+
     message.save((err, messageStored) => {
-       if(err) return res.status(500).send({message: 'Error en la peticion'}); 
-       if(!messageStored) return res.status(500).send({message: 'Error al enviar el mensaje'});
-       
-       res.status(200).send({message: messageStored});
+        if (err)
+            return res.status(500).send({message: 'Error en la peticion'});
+        if (!messageStored)
+            return res.status(500).send({message: 'Error al enviar el mensaje'});
+
+        res.status(200).send({message: messageStored});
     });
 
 }
+
+function getReceivedMessages(req, res) {
+    var userId = req.user.sub;
+
+    var page = 1;
+    if (req.params.page) {
+        page = req.params.page;
+    }
+    var itemsPerPage = 4;
+
+    Message.find({receiver: userId}).populate('emitter').paginate(page, itemsPerPage, (err, messages, total) => {
+        if (err)
+            return res.status(500).send({message: 'Error en la peticion'});
+        if (!messages)
+            return res.status(404).send({message: 'No hay mensajes'});
+
+        return res.status(200).send({
+            total: total,
+            pages: Math.ceil(total / itemsPerPage),
+            messages
+        });
+
+    });
+}
 module.exports = {
     probando,
-    saveMessage
+    saveMessage,
+    getReceivedMessages
 }
